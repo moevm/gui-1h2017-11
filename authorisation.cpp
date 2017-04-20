6a79cc2d89cc9ae7e76c6c981e639c069d418e8d
anaraSeishin <anara.75197@gmail.com>
2017-04-20 09:17:15 +0300

    создано окно авторизации

---
+++ authorisation.cpp
@@ -0,0 +1,6 @@
+#include "authorisation.h"
+
+Authorisation::Authorisation()
+{
+
+}
---
+++ authorisation.h
@@ -0,0 +1,11 @@
+#ifndef AUTHORISATION_H
+#define AUTHORISATION_H
+
+
+class Authorisation
+{
+public:
+    Authorisation();
+};
+
+#endif // AUTHORISATION_H
